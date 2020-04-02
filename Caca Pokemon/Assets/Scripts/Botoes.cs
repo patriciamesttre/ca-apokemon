93af178978dcee89c1ae509fee045104143aeef9
patriciamesttre <patriciamesttre@hotmail.com>
2020-04-01 23:41:02 -0300

    Criado Tela Inicio, Tela Tutorial,Tela Configurações, Tela Ranking,Tela Fim de Jogo, Tela Jogo e Botões,

---
+++ Caca Pokemon/Assets/Scripts/Botoes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botoes : MonoBehaviour
+{
+   public void AbrirConfiguracoes()
+    {
+        SceneManager.LoadScene("Cenaconfiguracoes");
+    }
+
+    public void AbrirInicio()
+    {
+        SceneManager.LoadScene("CenaInicio");
+    }
+
+    public void AbrirJogo()
+    {
+        SceneManager.LoadScene("CenaJogo");
+    }
+
+    public void AbrirTutorial()
+    {
+        SceneManager.LoadScene("CenaTutorial");
+    }
+
+    public void AbrirRanking()
+    {
+        SceneManager.LoadScene("CenaRanking");
+    }
+
+    public void AbrirFim()
+    {
+        SceneManager.LoadScene("CenaFim");
+    }
+
+    public void EncerrarJogo()
+    {
+        Application.Quit();
+    }
+}
